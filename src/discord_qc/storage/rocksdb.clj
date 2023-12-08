b82153a207fcdc2329c9b8330657ac4e7f53227e
Barak <barakx.oron@intel.com>
2023-12-08 11:03:34 +0200

    rocksdb

--- src/discord_qc/storage/rocksdb.clj
+++ src/discord_qc/storage/rocksdb.clj
@@ -6,40 +6,7 @@
            [org.rocksdb Options]))
 
 
-(defonce ^:private db (atom nil))
-
-
-(defonce status* (atom {:status :off
-                        :config {:db-path "/tmp/rocksdb"}}))
-
-
-(defn is-alive? []
-  (= :on (:status @status*)))
-
-
-(defn- init-rocksdb! []
-  (let [db-path (get-in @status* [:config :db-path])
-        opts (-> (Options.)
-               (.setCreateIfMissing true)
-               (.setErrorIfExists false))
-        db   (RocksDB/open opts db-path)]
-    db))
-
-
-(defn start! [ & {:keys [config] :or {config {:db-path "/tmp/rocksdb"}}}] ;;this dir is set as a docker volume in compose for persistency
-                     
-  (try
-    (when (not-empty config)
-      (swap! status* assoc :config config))
-    (when (not (is-alive?)) ;; otherwise will fail if already up
-      (reset! db (init-rocksdb!)))
-    (println "[storage.rocksdb]: started" config)
-    (swap! status* assoc :status :on)
-    :on
-    (catch Exception e (do 
-                         (println "[storage.rocksdb]" e)
-                         (swap! status* assoc :status :error)
-                         :error))))
+(defonce ^:private db* (atom nil))
 
 
 (defn- rocksdb-serialize-value [value]
@@ -47,23 +14,42 @@
 
 
 (defn- rocksdb-deserialize-value [value]
-   (if-let [val (bs/to-string value)]
+   (when-let [val (bs/to-string value)]
      (try 
        (edn/read-string val)
-       (catch Exception e val))
-     nil))
+       (catch Exception _ val))))
 
 
-(defn put-record! [k v]  
-  (when (is-alive?)
+(defn put-record! [k v]
+  (when-let [db @db*]
     (println "[storage.rocksdb]: put into key: " k)
-    (.put @db (rocksdb-serialize-value k) (rocksdb-serialize-value v))))
+    (.put db (rocksdb-serialize-value k) (rocksdb-serialize-value v))))
 
 
 (defn get-record [k]
-  (when (is-alive?)
-    (if-let [byte-array-val (.get @db (rocksdb-serialize-value k))]
-      (rocksdb-deserialize-value byte-array-val) 
-      nil)))
+  (when-let [db @db*]
+    (some->> k
+      (rocksdb-serialize-value)
+      (.get db)
+      (rocksdb-deserialize-value))))
 
-(start!)
+
+(defn stop! []
+  (when-let [db @db*]
+    (reset! db* nil)
+    (.stop db)))
+
+
+(defn start! [{db-path :db-path}]
+  (when @db*
+    (stop!))
+
+  (let [opts (-> (Options.)
+               (.setCreateIfMissing true)
+               (.setErrorIfExists   false))]
+
+    (reset! db* (RocksDB/open opts db-path))
+    (println "[storage.rocksdb]: started " db-path)))
+
+
+(start! {:db-path "/tmp/rocksdb"})
