9627c06de429abefa237f0777b95c7b00c76085a
barak <barakor@users.noreply.github.com>
2025-02-01 01:06:05 +0200

    log voice-states

--- src/discord_qc/discord/events.clj
+++ src/discord_qc/discord/events.clj
@@ -1,21 +1,19 @@
 (ns discord-qc.discord.events
   (:require
-    [discljord.connections :as discord-ws]
-    [slash.core :as sc]
-    [discljord.events.state :as discord-state]
+   [discljord.connections :as discord-ws]
+   [slash.core :as sc]
+   [discljord.events.state :as discord-state]
 
-    [discord-qc.state :refer [state* config discord-state*]]
-    [discord-qc.discord.interactions :refer [interaction-handlers]]
-    [discord-qc.discord.interactions.message :refer [balance-pubobot-queue]]
-    
-    [taoensso.timbre :as timbre :refer [log]]
-    [com.rpl.specter :as s]))
-   
+   [discord-qc.state :refer [state* config discord-state*]]
+   [discord-qc.discord.interactions :refer [interaction-handlers]]
+   [discord-qc.discord.interactions.message :refer [balance-pubobot-queue]]
+
+   [taoensso.timbre :as timbre :refer [log]]
+   [com.rpl.specter :as s]))
 
 (defmulti handle-event
   "Event handling multi method. Dispatches on the type of the event."
   (fn [type _data] type))
-
 
 (defmethod handle-event :message-create [_ {:keys [channel-id author embeds mentions] :as event-data}]
   (try
@@ -31,32 +29,29 @@
 
 (defmethod handle-event :default [type data])
 
-
 (defmethod handle-event :interaction-create
   [_ event-data]
   (sc/route-interaction interaction-handlers event-data))
 
-
 (defmethod handle-event :guild-create [type data]
+  (log :debug (:voice-states data))
   (let [register-user-voice-channel (fn [{:keys [channel-id user-id]}]
-                                      (swap! discord-state* 
-                                             update-in [:voice-channels channel-id] 
-                                                       #(clojure.set/union % #{user-id})))
-  
+                                      (swap! discord-state*
+                                             update-in [:voice-channels channel-id]
+                                             #(clojure.set/union % #{user-id})))
+
         voice-channels-users (s/select [:voice-states s/ALL (s/submap [:channel-id :user-id])] data)]
     (doall (map register-user-voice-channel voice-channels-users))))
-    
 
 (defn voice-state-channel-update [_ {:keys [user-id guild-id channel-id] :as voice} state*]
   (when-let [old-channel-id (get-in @state* [:discljord.events.state/users user-id :voice :channel-id])]
     (swap! state* update-in [:voice-channels  old-channel-id] #(clojure.set/difference % #{user-id})))
   (when channel-id
     (swap! state* update-in [:voice-channels channel-id] #(clojure.set/union % #{user-id}))))
-  
-(defn voice-state-update-wrapper [ _ voice state*]
+
+(defn voice-state-update-wrapper [_ voice state*]
   (voice-state-channel-update  _ voice state*)
   (discord-state/voice-state-update _ voice state*))
-
 
 ;TODO : ADD A :guild-create event wrapper to start the :voice-channels state
 
